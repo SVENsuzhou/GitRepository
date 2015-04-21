b7d6ca14dcaeeaee5a5624680512396616098e9b
SVENsuzhou <1245366017@qq.com>
2015-04-21 16:45:49 +0800

    回档脚本

--- LinuxShell/backRecord.sh
+++ LinuxShell/backRecord.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 #author:zhu huifeng
 
+#声明变量
+User="ljc"
+Passwd="xxxxx"
+Host="192.168.0.110"
+DataBase="db_public"
+
+#判断参数是否正确
 if [ -z $1 ] || [ -z $2 ]; then
 	echo "Usage: $0 OldCharid NewCharid"
 	exit 1;
 fi
 
 #查看新数据是否存在
-value=`\mysql -u ljc -pxxxxxx -h 192.168.0.110 -e "use db_public; select charid from CHARBASE where charid = $2;"`
+value=`\mysql -u $User -p$Passwd -h $Host -e "use $DataBase; select charid from CHARBASE where charid = $2;"`
 if [ -n "$value" ]; then
     echo "CHARID $2 已经存在，请手动处理"
     exit 2;
 fi
 
+#备份旧数据
+if [ ! -e /tmp/$1.sql.bak ]; then
+    \mysql -u $User -p$Passwd -h $Host -e "use $DataBase; select * from CHARBASE where charid = $1 into outfile \"/tmp/$1.sql.bak\";"
+fi
+
 #加载数据
-\mysql -u ljc -pxxxxxx -h 192.168.0.110 -e "use db_public; load data infile \"/tmp/$2.sql\" into table CHARBASE;"
+\mysql -u $User -p$Passwd -h $Host -e "use $DataBase; load data infile \"/tmp/$2.sql\" into table CHARBASE;"
 
 #创建存储过程
-\mysql -u ljc -pxxxxxx -h 192.168.0.110 << EOF
-use db_public
+\mysql -u $User -p$Passwd -h $Host << EOF
+use $DataBase
 drop PROCEDURE if exists BackRecord;
 delimiter $$
 create PROCEDURE BackRecord(OldCharid int, NewCharid int)
@@ -38,6 +50,6 @@ delimiter ;
 EOF
 
 #调用存储过程
-\mysql -u ljc -pxxxxxx -h 192.168.0.110 -e "use db_public; call BackRecord($1, $2);"
+\mysql -u $User -p$Passwd -h $Host -e "use $DataBase; call BackRecord($1, $2);"
 
 
