88c7055b76da8fe573596a2171f38cefff8fb2c5
SVENsuzhou <1245366017@qq.com>
2015-07-09 17:08:54 +0800

    修改stop脚本

--- LinuxShell/stop.sh
+++ LinuxShell/stop.sh
@@ -5,16 +5,25 @@
 
 my_stop()
 {
-for I in 'GatewayServer' 'LogicServer' 'RecordServer' 'GlobalServer'; do
-    for ID in `ps x | grep $I | grep -v grep | awk '{print $1}'`; do
-        COMMAND() { kill -2 $ID; }
-        if [ $I = 'RecordServer' ]; then
-            sleep 60
-        else
-            sleep 2
-        fi
-        action "stop $I service:" COMMAND
+local -a MyPid
+local i=0
+for I in 'GatewayServer -d' 'LogicServer -d' 'RecordServer -d' 'GlobalServer -d'; do
+    for ID in `ps x | grep "$I" | grep -v grep | awk '{print $1}'`; do
+        MyPid[$i]=$ID
+        let i=$i+1
     done
+    if [ ${#MyPid[*]} -eq 0 ]; then
+        continue
+    fi
+    COMMAND() { kill -2 ${MyPid[@]}; }
+    if [ "$I" = 'RecordServer -d' ]; then
+        sleep 10
+    else
+        sleep 2
+    fi
+    action "stop ${I:0:${#I}-3} service:" COMMAND
+    unset MyPid[@]
+    let i=0
 done
 unset COMMAND
 }
@@ -22,10 +31,10 @@ unset COMMAND
 pid_test()
 {
 sleep 5
-for I in 'GatewayServer' 'LogicServer' 'RecordServer' 'GlobalServer'; do
-    ps x | grep -v grep | grep $I > /dev/null
+for I in 'GatewayServer -d' 'LogicServer -d' 'RecordServer -d' 'GlobalServer -d'; do
+    ps x | grep -v grep | grep "$I" > /dev/null
     if [ $? -eq 0 ]; then
-          echo "Warning: $I 进程没有结束，请手动终结该进程"
+          echo -e "Warning: ${I:0:${#I}-3}\t进程没有结束，请手动终结该进程"
     fi
 done
 }
