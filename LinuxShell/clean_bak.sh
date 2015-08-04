c4e5a0b45e8aca995e96c03096ae7faec6af691c
SVENsuzhou <1245366017@qq.com>
2015-08-04 11:14:32 +0800

    修改备份打包脚本

--- LinuxShell/clean_bak.sh
+++ LinuxShell/clean_bak.sh
@@ -2,34 +2,36 @@
 #author: zhu huifeng
 trap "exec 1000>&- && exec 1000<&- && exit 1" SIGINT
 
-#关联命名管道和文件描述符
+#关联管道和文件描述符
 fifofile="/tmp/$$.fifo"
 /bin/mknod $fifofile p
 exec 1000<>$fifofile
 rm $fifofile
 
-#定义进程数
 thread=10
 for ((i=0;i<$thread;i++)); do
     echo >& 1000
 done
 
-#将区名字填入MYZONE数组中
+#将区填入MYZONE数组中
 DIR="/db/backup/"
+DAYBAK="/db/daybackup/"
+[ -d $DAYBAK ] || mkdir $DAYBAK       #检查每日备份文件夹是否存在
+
 declare -a MYZONE
 declare -i M=0
 for ZONE in `/bin/find $DIR -name \*-$(/bin/date +%Y%m%d)_\*.sql | grep -o record.*- | awk -F '-' '{print $1}' | sort -u`; do
     MYZONE[$M]=$ZONE
     let M=$M+1
-done 
+done
 
 #开始多进程打包
 for ((i=0;i<$M;i++)); do
     read -u 1000
     {
-	cd $DIR
-        /bin/find . -name ${MYZONE[$i]}-`/bin/date --date='1 days ago' +%Y%m%d`_\*.sql -exec basename {} \; | sort | xargs tar zcf ${MYZONE[$i]}-`/bin/date --date='1 days ago' +%Y%m%d`.tar.gz --remove-files
-	echo >& 1000
+        cd $DIR
+        /bin/find . -name ${MYZONE[$i]}-`/bin/date --date='1 days ago' +%Y%m%d`_\*.sql -exec basename {} \; | sort | xargs tar zcf ${DAYBAK}${MYZONE[$i]}-`/bin/date --date='1 days ago' +%Y%m%d`.tar.gz --remove-files
+        echo >& 1000
     } &
 done
 wait
@@ -37,7 +39,7 @@ wait
 #获取备份文件的日期
 declare -a MyDate
 declare -i n=0
-for I in `ls $DIR | grep [0-9]*\.tar\.gz | cut -d '.' -f 1 | cut -d '-' -f 2`; do 
+for I in `ls $DAYBAK | grep [0-9]*\.tar\.gz | cut -d '.' -f 1 | cut -d '-' -f 2 | sort -u`; do
     MyDate[$n]=$I
     let n=$n+1
 done
@@ -49,18 +51,18 @@ done
 #done
 #}
 
-#当磁盘空间小于某个值时，将最早的5个日期的备份文件删除
+#超出阈值则删除一部分备份文件
 declare -i real_num
 declare -i disk_num
-real_num=`du -s $DIR | cut -f 1`
+real_num=`du -s /db/ | cut -f 1`
 disk_num=60000000
 if [ $real_num -gt $disk_num ]; then
-   for I in `seq 0 4`; do
-      /bin/rm -f $DIR*-${MyDate[$I]}.tar.gz
+   for I in `seq 0 2`; do
+      /bin/rm -rf $DAYBAK*-${MyDate[$I]}.tar.gz
    done
 fi
 
-#取消文件描述符的关联
+
 exec 1000>&-
 exec 1000<&-
 
